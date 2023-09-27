942ee1caab8d551aa2d21ae76a659abdf114d677
Karl Gluck <karlgluck@users.noreply.github.com>
2023-09-27 11:23:13 -0700

    hack to include Expand-ZipFileDirectory

--- Import-GameJamSetup.ps1
+++ Import-GameJamSetup.ps1
@@ -49,3 +49,48 @@ function Import-GameJamSetup
     Import-Module GameJamSetup -Force
 
 }
+
+# what? this totally isn't a hack to bootstrap the first launch...
+
+<#
+.DESCRIPTION
+  Extracts all contents of a sub-directory in a ZIP file into a target directory
+#>
+function Expand-ZipFileDirectory
+{
+  Param(
+    [Parameter(Mandatory)]
+    [string]$ZipFilePath,
+
+    [Parameter(Mandatory)]
+    [string]$DirectoryInZipFile,
+
+    [Parameter(Mandatory)]
+    [string]$OutputPath
+  )
+
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  $DirectoryRegex = "\/" + ($DirectoryInZipFile.Trim('/\') -replace "/","\/") + "\/(.*)"
+  try
+  {
+    $Zip = [System.IO.Compression.ZipFile]::OpenRead($ZipFilePath)
+    $Zip.Entries | 
+      Where-Object { $_.Name -ne "" } |
+      ForEach-Object {
+        $Match = [RegEx]::Match($_.FullName, $DirectoryRegex)
+        if ($Match.Success)
+        {
+          $OutputFilePath = Join-Path $OutputPath $Match.Groups[1].Value
+          $OutputDirectoryPath = Split-Path -Parent $OutputFilePath
+          Write-Host " > Extracting $($_.FullName)"
+          if (-not (Test-Path $OutputDirectoryPath)) { New-Item $OutputDirectoryPath -ItemType Directory | Out-Null }
+          [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $OutputFilePath, $true) | Out-Null
+        }
+      }
+  }
+  finally
+  {
+    $Zip.Dispose()
+  }
+
+}
